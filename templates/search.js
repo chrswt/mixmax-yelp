bb5b5f70d1887eee24edafd0bc53dfce0ce852b1
Christopher Tham <chriswt91@gmail.com>
2017-02-17 21:48:09 -0800

    Fix template rendering of search items

--- templates/search.js
+++ templates/search.js
@@ -3,12 +3,16 @@ const generateTemplate = (name, rating, address) => {
 
   // Generate template for search predictions
   return `
-    <div style="font-weight: 300; font-family: 'Helvetica Neue', sans-serif;">
-      <div style="font-size: 16px">
-        ${name}
+    <div style="font-weight: 300; font-family: 'Helvetica Neue', sans-serif; text-overflow: ellipsis; overflow: hidden; white-space: nowrap;">
+      <div style="font-size: 16px; max-height: 24px">
+        <div style="max-width: 200px; display: inline-block; text-overflow: ellipsis; overflow: hidden;">
+          ${name}
+        </div>
         <img src="${rating}" align="right"></img>
       </div>
-      <div style="font-size: 11px">${address}</div>
+      <div style="font-size: 11px;">
+        ${address}
+      </div>
     </div>`;
 };
 
