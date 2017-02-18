efbc2156b98c762f09ac0b7d4a95c3c236af451f
Christopher Tham <chriswt91@gmail.com>
2017-02-17 20:25:27 -0800

    Add Yelp API keys structure

---
+++ utils/_yelp.js
@@ -0,0 +1,8 @@
+const Yelp = require('yelp');
+
+module.exports = new Yelp({
+  consumer_key: 'Add consumer key',
+  consumer_secret: 'Add consumer secret',
+  token: 'Add token',
+  token_secret: 'Add token secret',
+});
